e3d958886c454c979159a7c176e4b3a6376ca9c2
hwana <dsa1461@gmail.com>
2021-01-26 23:28:37 +0900

    구글 로그인 적용

--- src/main/java/com/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/book/springboot/service/posts/PostsService.java
@@ -18,10 +18,13 @@ import java.util.stream.Collectors;
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    //등록
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    //수정
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
@@ -29,17 +32,20 @@ public class PostsService {
         return id;
     }
 
+    //1개 조회
     @Transactional
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
 
+    //전체 조회
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
+    //삭제
     @Transactional
     public void delete(Long id){
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.book.springboot.web;
 
+import com.book.springboot.config.auth.dto.SessionUser;
 import com.book.springboot.service.posts.PostsService;
 import com.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,28 +9,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
+    //메인페이지
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
+    //등록페이지
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
+    //수정페이지
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
+
 
 }
