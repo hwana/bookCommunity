35546becf7829c1901e1ab8c1eb7b7e744b8e315
hwana <dsa1461@gmail.com>
2021-02-08 20:45:58 +0900

    deploy.sh, .travis.yml 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=bookCommunity
+PROJECT_NAME=community
 
 echo "> Build 파일 복사"
 
@@ -8,7 +8,7 @@ cd $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl bookCommunity | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl community | grep jar | awk '{print $1}')
 
 echo >"현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
